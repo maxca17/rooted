1b2cfaa97c30f9c0d66dc7542fe7a32409b1ee82
maxca17 <maxcalvert34@gmail.com>
2024-10-26 17:33:05 -0500

    updates for our stats

--- src/components/event-content-card.tsx
+++ src/components/event-content-card.tsx
@@ -28,11 +28,17 @@ export function EventContentCard({
       color="transparent"
       shadow={false}
       className="lg:!flex-row mb-10 lg:items-end"
+      placeholder=""
+      onPointerEnterCapture={() => {}}
+      onPointerLeaveCapture={() => {}}
     >
       <CardHeader
         floated={false}
         shadow={false}
         className="h-[32rem] max-w-[28rem] shrink-0"
+        placeholder=""
+        onPointerEnterCapture={() => {}}
+        onPointerLeaveCapture={() => {}}
       >
         <Image
           width={768}
@@ -42,14 +48,19 @@ export function EventContentCard({
           className="h-full w-full object-cover"
         />
       </CardHeader>
-      <CardBody className="col-span-full lg:col-span-3">
-        <Typography variant="h6" color="blue-gray" className="mb-4">
+      <CardBody 
+        className="col-span-full lg:col-span-3"
+        placeholder=""
+        onPointerEnterCapture={() => {}}
+        onPointerLeaveCapture={() => {}}
+      >
+        <Typography variant="h6" color="blue-gray" className="mb-4" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
           {panel}
         </Typography>
-        <Typography variant="h2" color="blue-gray" className="mb-4 font-medium">
+        <Typography variant="h2" color="blue-gray" className="mb-4 font-medium" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
           {title}
         </Typography>
-        <Typography className="mb-12 md:w-8/12 font-medium !text-gray-500">
+        <Typography className="mb-12 md:w-8/12 font-medium !text-gray-500" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
           {des}
         </Typography>
         <div className="flex items-center gap-4">
@@ -58,12 +69,15 @@ export function EventContentCard({
             src="/logos/spotify-logo.png"
             alt="spotify"
             size="lg"
+            placeholder=""
+            onPointerEnterCapture={() => {}}
+            onPointerLeaveCapture={() => {}}
           />
           <div>
-            <Typography variant="h6" color="blue-gray" className="mb-0.5">
+            <Typography variant="h6" color="blue-gray" className="mb-0.5" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
               {name}
             </Typography>
-            <Typography variant="small" className="font-normal !text-gray-500">
+            <Typography variant="small" className="font-normal !text-gray-500" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
               {position}
             </Typography>
           </div>
