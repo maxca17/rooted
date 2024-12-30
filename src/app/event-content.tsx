95ec04b871681b31f7335e5557c3282ffc13502c
maxca17 <maxcalvert34@gmail.com>
2024-12-29 22:23:04 -0600

    test

--- src/app/event-content.tsx
+++ src/app/event-content.tsx
@@ -1,20 +1,14 @@
-// EventContent.jsx
-
 "use client";
 
 import React from "react";
 import { Typography, Button } from "@material-tailwind/react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Image from "next/image";
-// @ts-ignore
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
 
-// Data for the Carousel
+// Data for the "carousel" (displayed as a list)
 const CAROUSEL_CONTENT = [
   {
-    des: "While technology has made the world smaller, there is no substitute for sharing a real, physical experience together. Rooted Expo brings the diverse flavors and styles crafted in every corner of Texas into one convenient setting where we can celebrate the excellence of our far-reaching CPG community. ",
+    des: "While technology has made the world smaller, there is no substitute for sharing a real, physical experience together. Rooted Expo brings the diverse flavors and styles crafted in every corner of Texas into one convenient setting where we can celebrate the excellence of our far-reaching CPG community.",
     name: "Jeff Richards",
     position: "CEO | Founder of Mooala",
     img: "/image/jeffr.png",
@@ -50,130 +44,83 @@ const boardMembers = [
   {
     name: "Derek Ramos",
     role: "Founder & Chief Organizer of Rooted Expo",
-    image: '/image/derrick.png', 
-    link: "https://tinyurl.com/2zaxuja3" 
+    image: "/image/derrick.png",
+    link: "https://tinyurl.com/2zaxuja3",
   },
   {
     name: "Kat Weaver",
     role: "Founder of Power to Pitch & Event Co-Chair of DFW CPG",
-    image: '/image/kat.png', 
-    link: "https://tinyurl.com/9kfde4cs"
+    image: "/image/kat.png",
+    link: "https://tinyurl.com/9kfde4cs",
   },
   {
     name: "Richard G. Riccardi",
     role: "Writer, and CPG Advocate & Co-Founder of DFW CPG",
-    image: '/image/richard.png', 
-    link: "https://tinyurl.com/mrys9736"
+    image: "/image/richard.png",
+    link: "https://tinyurl.com/mrys9736",
   },
   {
     name: "Felipe Vega",
     role: "Founder of IronClad & Board President of Naturally Austin",
-    image: '/image/f.png', 
-    link: "https://tinyurl.com/39jf9zja"
-  }
+    image: "/image/f.png",
+    link: "https://tinyurl.com/39jf9zja",
+  },
 ];
 
 const teamMembers = [
   {
     name: "Katrina Tolentino",
     role: "Executive Director of Naturally Network",
-    image: '/image/katr.png',
-    link: "https://www.linkedin.com/in/katrinatolentino" 
+    image: "/image/katr.png",
+    link: "https://www.linkedin.com/in/katrinatolentino",
   },
   {
     name: "Rick Jordan",
     role: "Managing Partner of Polsinelli & Co-Founder of DFW CPG",
-    image: '/image/rick.png', 
-    link: "https://www.linkedin.com/in/rick-jordan-81b38316"
+    image: "/image/rick.png",
+    link: "https://www.linkedin.com/in/rick-jordan-81b38316",
   },
   {
     name: "Jeff Richards",
     role: "Founder of Mooala",
-    image: '/image/jeff.png', 
-    link: "https://www.linkedin.com/in/jeffreyrichards"
+    image: "/image/jeff.png",
+    link: "https://www.linkedin.com/in/jeffreyrichards",
   },
   {
     name: "Marc Nathan",
     role: "Professional 'Super Connector' for Tech & CPG companies",
-    image: '/image/marc.png', 
-    link: "https://www.linkedin.com/in/marc1919/"
-  }
+    image: "/image/marc.png",
+    link: "https://www.linkedin.com/in/marc1919/",
+  },
 ];
-
-interface ArrowProps {
-  onClick: () => void;
-}
-
-function PrevArrow({ onClick }: ArrowProps) {
-  return (
-    <button
-      onClick={onClick}
-      className="absolute top-1/2 left-4 transform -translate-y-1/2 p-2 bg-black rounded-full shadow-lg focus:outline-none z-10"
-      style={{ transform: "translateY(-50%)" }}
-    >
-      <i className="fas fa-chevron-left fa-lg text-white"></i>
-    </button>
-  );
-}
-
-function NextArrow({ onClick }: ArrowProps) {
-  return (
-    <button
-      onClick={onClick}
-      className="absolute top-1/2 right-4 transform -translate-y-1/2 p-2 bg-black rounded-full shadow-lg focus:outline-none z-10"
-      style={{ transform: "translateY(-50%)" }}
-    >
-      <i className="fas fa-chevron-right fa-lg text-white"></i>
-    </button>
-  );
-}
-
 export default function EventContent() {
-  const settings = {
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    speed: 1000,
-    fade: true,
-    arrows: true,
-    prevArrow: <PrevArrow onClick={() => {}} />,
-    nextArrow: <NextArrow onClick={() => {}} />,
-    pauseOnHover: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
     <section className="py-8 px-8 lg:py-20 bg-white">
-      {/* Carousel */}
+      {/* Carousel Content - Displayed as vertical list */}
       <div className="mx-auto container">
-        <div className="mt-12 relative">
-          <Slider {...settings}>
-            {CAROUSEL_CONTENT.map((item, idx) => (
-              <div
-                key={idx}
-                className="flex flex-col items-center justify-center min-h-[400px] text-center bg-white"
-              >
-                <div className="flex flex-col items-center justify-center px-4">
-                  <Image
-                    src={item.img}
-                    className="rounded-full mb-4 shadow-lg object-cover"
-                    alt={`${item.name} avatar`}
-                    width={90}
-                    height={90}
-                  />
-                  <p className="italic text-lg text-gray-800 max-w-2xl mx-auto">
-                    <i className="fas fa-quote-left fa-lg text-gray-500 mr-2"></i>
-                    {item.des}
-                  </p>
-                  <h5 className="mt-4 text-xl font-semibold text-black">
-                    {item.name}
-                  </h5>
-                  <p className="text-gray-600">{item.position}</p>
-                </div>
-              </div>
-            ))}
-          </Slider>
+        <div className="mt-12 space-y-8">
+          {CAROUSEL_CONTENT.map((item, idx) => (
+            <div
+              key={idx}
+              className="flex flex-col items-center justify-center min-h-[200px] text-center bg-white"
+            >
+              <Image
+                src={item.img}
+                className="rounded-full mb-4 shadow-lg object-cover"
+                alt={`${item.name} avatar`}
+                width={90}
+                height={90}
+              />
+              <p className="italic text-lg text-gray-800 max-w-2xl mx-auto">
+                <i className="fas fa-quote-left fa-lg text-gray-500 mr-2"></i>
+                {item.des}
+              </p>
+              <h5 className="mt-4 text-xl font-semibold text-black">
+                {item.name}
+              </h5>
+              <p className="text-gray-600">{item.position}</p>
+            </div>
+          ))}
         </div>
       </div>
 
@@ -181,9 +128,9 @@ export default function EventContent() {
       <section className="py-16 px-8 bg-white">
         {/* Section Title */}
         <div className="relative mb-12 text-center">
-          <Typography
-            variant="h2"
-            color="black"
+          <Typography 
+            variant="h2" 
+            color="black" 
             className="text-4xl font-bold mb-4"
             placeholder=""
             onPointerEnterCapture={() => {}}
@@ -220,10 +167,21 @@ export default function EventContent() {
                     height={120}
                     className="rounded-full mx-auto mb-4 object-cover"
                   />
-                  <Typography variant="h5" className="font-bold mb-2 text-black" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
+                  <Typography 
+                    variant="h5" 
+                    className="font-bold mb-2 text-black"
+                    placeholder=""
+                    onPointerEnterCapture={() => {}}
+                    onPointerLeaveCapture={() => {}}
+                  >
                     {member.name}
                   </Typography>
-                  <Typography className="text-gray-700 mb-4" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
+                  <Typography 
+                    className="text-gray-700 mb-4"
+                    placeholder=""
+                    onPointerEnterCapture={() => {}}
+                    onPointerLeaveCapture={() => {}}
+                  >
                     {member.role}
                   </Typography>
                   <Button
@@ -267,10 +225,21 @@ export default function EventContent() {
                     height={120}
                     className="rounded-full mx-auto mb-4 object-cover"
                   />
-                  <Typography variant="h5" className="font-bold mb-2 text-black" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
+                  <Typography 
+                    variant="h5" 
+                    className="font-bold mb-2 text-black"
+                    placeholder=""
+                    onPointerEnterCapture={() => {}}
+                    onPointerLeaveCapture={() => {}}
+                  >
                     {member.name}
                   </Typography>
-                  <Typography className="text-gray-700 mb-4" placeholder="" onPointerEnterCapture={() => {}} onPointerLeaveCapture={() => {}}>
+                  <Typography 
+                    className="text-gray-700 mb-4"
+                    placeholder=""
+                    onPointerEnterCapture={() => {}}
+                    onPointerLeaveCapture={() => {}}
+                  >
                     {member.role}
                   </Typography>
                   <Button
