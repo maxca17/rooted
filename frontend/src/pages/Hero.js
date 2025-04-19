13f48102431316bba01ccc14daeb7a968c979de7
maxca17 <maxcalvert34@gmail.com>
2025-04-18 19:17:49 -0500

    fixed dates

--- frontend/src/pages/Hero.js
+++ frontend/src/pages/Hero.js
@@ -5,14 +5,14 @@ function Hero() {
       <section className="hero-section">
         <div className="hero-overlay" />
         <div className="hero-content">
-          <h3>Sept 30th - Oct 2nd @ Waco, Texas</h3>
-          <h1>Rooted Expo 2025: Bringing Texas CPG Together</h1>
-          <p>Join us for the most anticipated event of the year - Rooted 2025!</p>
+          <h3>CITY ANNOUNCEMENT COMING SOON</h3>
+          <h1>Rooted Expo: Bringing Texas CPG Together</h1>
+          <p>Join us for the most anticipated event of the year</p>
           <button
             className="hero-button"
             onClick={() => (window.location.href = "/waitlist")}
           >
-            Join 2025 Waitlist
+            Join Waitlist
           </button>
         </div>
       </section>
